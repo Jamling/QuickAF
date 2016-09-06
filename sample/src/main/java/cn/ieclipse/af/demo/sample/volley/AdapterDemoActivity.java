19837e44ab075aea0ccc063b6c47faf41e85293b
wangjian <453191304@qq.com>
2016-09-06 09:45:44 +0800

    add gson parser adapter demo

--- sample/src/main/java/cn/ieclipse/af/demo/sample/volley/AdapterDemoActivity.java
+++ sample/src/main/java/cn/ieclipse/af/demo/sample/volley/AdapterDemoActivity.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2015-2016 QuickAF
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package cn.ieclipse.af.demo.sample.volley;
 
 import android.view.View;
@@ -15,7 +30,7 @@ import cn.ieclipse.af.volley.VolleyManager;
  * 类/接口描述
  *
  * @author wangjian
- * @date 2016/9/2.
+ * @date 2016-08-08.
  */
 public class AdapterDemoActivity extends BaseActivity {
 
@@ -63,6 +78,7 @@ public class AdapterDemoActivity extends BaseActivity {
     }
 
     private void parseJson() {
+        // 获得jsonParser
         User user = VolleyManager.getConfig().getJsonParser().fromJson(json, User.class);
         tvParser.setText(user.toString());
     }
