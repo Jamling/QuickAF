19552beb4379f39bb0ea3cccbaace2f2805d5ba6
Jamling <li.jamling@gmail.com>
2017-05-10 10:20:46 +0800

    add scroll text sample

--- app/src/main/java/cn/ieclipse/af/demo/sample/cview/TextViewSample.java
+++ app/src/main/java/cn/ieclipse/af/demo/sample/cview/TextViewSample.java
@@ -1,5 +1,8 @@
 package cn.ieclipse.af.demo.sample.cview;
 
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+
 import cn.ieclipse.af.demo.R;
 import cn.ieclipse.af.demo.sample.SampleBaseFragment;
 
@@ -13,5 +16,11 @@ public class TextViewSample extends SampleBaseFragment {
     @Override
     protected int getContentLayout() {
         return R.layout.sample_cview_text_view;
+    }
+    
+    @Override
+    protected void initContentView(View view) {
+        super.initContentView(view);
+        tv1.setMovementMethod(ScrollingMovementMethod.getInstance());
     }
 }
