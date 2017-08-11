b2313135ec7a9a82e5a602aa466940336d66aad4
Jamling <li.jamling@gmail.com>
2017-08-11 16:17:45 +0800

    remove webview onscroll listener

--- library/src/cn/ieclipse/af/view/refresh/RefreshWebViewDetector.java
+++ library/src/cn/ieclipse/af/view/refresh/RefreshWebViewDetector.java
@@ -1,7 +1,6 @@
 package cn.ieclipse.af.view.refresh;
 
 import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.view.View;
 import android.webkit.WebView;
 
@@ -13,25 +12,36 @@ import android.webkit.WebView;
  */
 public class RefreshWebViewDetector extends RefreshLayout.RefreshDetector<WebView> {
 
-    @RequiresApi(Build.VERSION_CODES.M)
-    private View.OnScrollChangeListener mOnScrollChangeListener = new View.OnScrollChangeListener() {
-        @Override
-        public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-            if (getRefresh().isEnableLoadMore()) {
-                int ch = (int) (getView().getContentHeight() * getView().getScale());
-                int vh = scrollY + v.getHeight();
-                if (vh + 2 >= ch) {
-                    getRefresh().loadMore();
-                }
-            }
-        }
-    };
+//    @RequiresApi(Build.VERSION_CODES.M)
+//    private View.OnScrollChangeListener mOnScrollChangeListener = new View.OnScrollChangeListener() {
+//        @Override
+//        public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+//            if (getRefresh().isEnableLoadMore()) {
+//                int ch = (int) (getView().getContentHeight() * getView().getScale());
+//                int vh = scrollY + v.getHeight();
+//                if (vh + 2 >= ch) {
+//                    getRefresh().loadMore();
+//                }
+//            }
+//        }
+//    };
 
     @Override
     public void setEnabled(boolean enable) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (enable) {
-                getView().setOnScrollChangeListener(mOnScrollChangeListener);
+                getView().setOnScrollChangeListener(new View.OnScrollChangeListener() {
+                    @Override
+                    public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                        if (getRefresh().isEnableLoadMore()) {
+                            int ch = (int) (getView().getContentHeight() * getView().getScale());
+                            int vh = scrollY + v.getHeight();
+                            if (vh + 2 >= ch) {
+                                getRefresh().loadMore();
+                            }
+                        }
+                    }
+                });
             }
             else {
                 getView().setOnScrollChangeListener(null);
